2775b5e546176fb212d6c7499d3f0df322021f26
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-12-27 23:26:49 +0100

    adds App and style

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
+import {
+  ImageBackground,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
+
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+import { styles } from "./style";
+
+const imgBg = require("./assets/App_BG.png");
+
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Regulat": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+//     "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+//     "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+//   });
+// };
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
+  //   );
+  // }
+  const keyboardHiden = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHiden}>
+      <View style={styles.container}>
+        <ImageBackground source={imgBg} resizeMode="cover" style={styles.imgBg}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "0"}
+          >
+            <LoginScreen
+              stateKeyboard={{ isShowKeyboard, setIsShowKeyboard }}
+              keyboardHiden={keyboardHiden}
+            />
+            {/* <RegistrationScreen
+              stateKeyboard={{ isShowKeyboard, setIsShowKeyboard }}
+              keyboardHiden={keyboardHiden}
+            /> */}
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
