41211da7566951a3e713bbd52576aea02e732c24
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-12-29 18:26:14 +0100

    adds screen Home

---
+++ Screens/Home/Home.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Home = () => {
+  return <Text> Home page</Text>;
+};
+
+export default Home;
---
+++ Screens/Home/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  avatar: { flex: 1, alignItems: "center" },
+  imgAvatar: {
+    position: "absolute",
+    top: 0,
+    width: 120,
+    height: 120,
+    transform: [{ translateY: -60 }],
+    borderRadius: 16,
+    backgroundColor: "#F6F6F6",
+  },
+  addAvatar: {
+    position: "absolute",
+    bottom: 14,
+    right: -12.5,
+    width: 25,
+    height: 25,
+    borderWidth: 1,
+    borderRadius: 50,
+  },
+});
